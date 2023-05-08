7f9a7914ff69e24ccb8929c632745e09804f514f
masak1112 <gongbing1112@gmail.com>
2023-05-08 09:32:43 +0200

    add network models

---
+++ models/diffusion_utils.py
@@ -0,0 +1,171 @@
+# SPDX-FileCopyrightText: 2021 Earth System Data Exploration (ESDE), Jülich Supercomputing Center (JSC)
+
+# SPDX-License-Identifier: MIT
+
+"""
+This implementation is based on two reference:
+1) https://huggingface.co/blog/annotated-diffusion
+2) https://github.com/Janspiry/Image-Super-Resolution-via-Iterative-Refinement/blob/master/model/ddpm_modules/diffusion.py
+1) is a simple, unconditional diffussion, while 2) is conditional diffusion
+"""
+
+__email__ = "b.gong@fz-juelich.de"
+__author__ = "Bing Gong"
+__date__ = "2022-11-28"
+
+
+import torch
+import torch.nn.functional as F
+from torch import nn, einsum
+from tqdm import tqdm
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+#############Define the schedules for T timestamps
+def cosine_beta_schedule(timesteps, s=0.008):
+    """
+    cosine schedule as proposed in https://arxiv.org/abs/2102.09672
+    """
+    steps = timesteps + 1
+    x = torch.linspace(0, timesteps, steps)
+    alphas_cumprod = torch.cos(((x / timesteps) + s) / (1 + s) * torch.pi * 0.5) ** 2
+    alphas_cumprod = alphas_cumprod / alphas_cumprod[0]
+    betas = 1 - (alphas_cumprod[1:] / alphas_cumprod[:-1])
+    return torch.clip(betas, 0.0001, 0.9999)
+
+def linear_beta_schedule(timesteps):
+    beta_start = 0.0001
+    beta_end = 0.02
+    return torch.linspace(beta_start, beta_end, timesteps)
+
+def quadratic_beta_schedule(timesteps):
+    beta_start = 0.0001
+    beta_end = 0.02
+    return torch.linspace(beta_start**0.5, beta_end**0.5, timesteps) ** 2
+
+def sigmoid_beta_schedule(timesteps):
+    beta_start = 0.0001
+    beta_end = 0.02
+    betas = torch.linspace(-6, 6, timesteps)
+    return torch.sigmoid(betas) * (beta_end - beta_start) + beta_start
+
+def extract(a, t, x_shape):
+    batch_size = t.shape[0]
+    a = a.to(device)
+    out = a.gather(-1, t.to(device))
+    return out.reshape(batch_size, *((1,) * (len(x_shape) - 1))).to(device)
+
+
+class GaussianDiffusion(nn.Module):
+    def __init__(self, conditional=False, schedule_opt="linear", timesteps=200, model=None):
+        super().__init__()
+        self.conditional = conditional
+        self.model = model
+        self.timesteps = timesteps
+
+        # define beta schedule
+        if schedule_opt == "linear":
+            self.betas = linear_beta_schedule(timesteps)
+        elif schedule_opt == "quad":
+            self.betas = quadratic_beta_schedule(timesteps)
+        elif schedule_opt == "cosine":
+            self.betas = cosine_beta_schedule(timesteps)
+        elif schedule_opt == "sigmoid":
+            self.betas = sigmoid_beta_schedule(timesteps)
+        else:
+            raise NotImplementedError
+        self.set_new_noise_schedule()
+
+    def set_new_noise_schedule(self):
+        alphas = 1. - self.betas
+        # Returns the cumulative product of elements of input in the dimension dim
+        alphas_cumprod = torch.cumprod(alphas, axis = 0)
+        self.alphas_cumprod_prev = F.pad(alphas_cumprod[:-1], (1, 0), value = 1.0)
+        self.sqrt_recip_alphas = torch.sqrt(1.0 / alphas)
+
+        # calculations for diffusion q(x_t | x_{t-1}) and others
+        self.sqrt_alphas_cumprod = torch.sqrt(alphas_cumprod)
+        self.sqrt_one_minus_alphas_cumprod = torch.sqrt(1. - alphas_cumprod)
+        # calculations for posterior q(x_{t-1} | x_t, x_0)
+        self.posterior_variance = self.betas * (1. - self.alphas_cumprod_prev) / (1. - alphas_cumprod)
+
+
+
+    def q_sample(self, x_start, t, noise=None):
+        if noise is None:
+            noise = torch.randn_like(x_start)
+
+        sqrt_alphas_cumprod_t = extract(self.sqrt_alphas_cumprod, t, x_start.shape)
+        sqrt_one_minus_alphas_cumprod_t = extract(
+            self.sqrt_one_minus_alphas_cumprod, t, x_start.shape
+        )
+
+        return sqrt_alphas_cumprod_t * x_start + sqrt_one_minus_alphas_cumprod_t * noise
+
+
+
+    @torch.no_grad()
+    def p_sample(self, x, t, t_index, condition_x=None):
+        betas_t = extract(self.betas, t, x.shape)
+        sqrt_one_minus_alphas_cumprod_t = extract(
+            self.sqrt_one_minus_alphas_cumprod, t, x.shape
+        )
+        sqrt_recip_alphas_t = extract(self.sqrt_recip_alphas, t, x.shape)
+
+        # Equation 11 in the paper
+        # Use our model (noise predictor) to predict the mean
+        if condition_x is not None:
+            # this is conditional diffussion
+            print("conditiona_x shape", condition_x.shape)
+            print("x shape is ", x.shape)
+            x_recon = self.model(torch.cat([condition_x, x], dim=1), t)
+        else:
+            x_recon = self.model(x, t)
+        model_mean = sqrt_recip_alphas_t * (
+                x - betas_t * x_recon / sqrt_one_minus_alphas_cumprod_t
+        )
+        posterior_variance_t = extract(self.posterior_variance, t, x.shape)
+        noise = torch.randn_like(x)
+        # No noise when t == 0
+        if t_index == 0:
+            return model_mean
+        else:
+            # Algorithm 2 line 4:
+            return model_mean + torch.sqrt(posterior_variance_t) * noise
+
+
+    @torch.no_grad()
+    def p_sample_loop(self, shape, x_in=None):
+        #device = next(self.model.parameters()).device
+
+        b = shape[0]
+        img = torch.randn(shape, device = device)
+        # start from pure noise (for each example in the batch)
+        imgs = []
+        #The following code get from reference 2). However, this is some difference.
+        #Need to furthe check. i do not understand why ret_img part
+        if x_in is None:
+            print("Start the p_sample_loop for non-conditional diffusion")
+            for i in tqdm(reversed(range(0, self.timesteps)),
+                          desc = 'sampling loop time step',
+                          total = self.timesteps):
+                img = self.p_sample(img, torch.full((b,),
+                                    i, device = device, dtype = torch.long), i)
+                imgs.append(img)
+            return imgs
+        else:
+            print("Start the p_sample_loop for conditional diffusion")
+            for i in tqdm(reversed(range(0, self.timesteps)),
+                          desc = 'sampling loop time step',
+                          total = self.timesteps):
+                img = self.p_sample(img, torch.full((b,), i, device = device, dtype = torch.long),
+                                    i, condition_x = x_in)
+                imgs.append(img)
+            return imgs
+
+    def sample(self, image_size=160, batch_size=16, channels=3, x_in=None):
+        if self.conditional:
+            print("images size in sample", image_size)
+            return self.p_sample_loop(shape=(batch_size, 1, image_size, image_size), x_in=x_in)
+        else:
+            return self.p_sample_loop(shape=(batch_size, channels, image_size, image_size))
---
+++ models/network_vit.py
@@ -0,0 +1,209 @@
+
+# SPDX-FileCopyrightText: 2021 Earth System Data Exploration (ESDE), Jülich Supercomputing Center (JSC)
+#
+# SPDX-License-Identifier: MIT
+
+
+__email__ = "b.gong@fz-juelich.de"
+__author__ = "Bing Gong"
+__date__ = "2022-07-20"
+
+"""
+The implementation is based on the tutorial from the following link
+https://towardsdatascience.com/implementing-visualttransformer-in-pytorch-184f9f16f632
+"""
+
+import torch
+import torch.nn as nn
+from torch import Tensor
+import torch.nn.functional as F
+from einops.layers.torch import Rearrange, Reduce
+from einops import rearrange, reduce, repeat
+
+class PatchEmbedding(nn.Module):
+
+    def __init__(self,
+                 in_channels: int = 8,
+                 patch_size: int = 4,
+                 emb_size: int = 64,
+                 enable_cnn: bool = False,
+                 img_size: int = 16):
+        super().__init__()
+        """
+        in_channels :  the number of variables/channles
+        patch_size  :  the size of each patch
+        emb_size    :  the embedding size
+        enable_cnn  :  if use convolutional network as projection, if false, using linear projection
+        """
+        self.patch_size = patch_size
+        if enable_cnn:
+            self.projection = nn.Sequential(
+            # using a conv layer instead of a linear one -> performance gains
+            nn.Conv2d(in_channels, emb_size, kernel_size=patch_size, stride=patch_size),
+            Rearrange('b e (h) (w) -> b (h w) e')
+            )
+
+        else:
+            self.projection = nn.Sequential(
+            # break-down the image in s1 x s2 patches and flat them
+            Rearrange('b c (h s1) (w s2) -> b (h w) (s1 s2 c)', s1=patch_size, s2=patch_size),
+            nn.Linear(patch_size * patch_size * in_channels, emb_size)
+            )
+            # I remove the cls embedding, since we do not have class labels in the data,
+            # I used learnable position embedding in this case
+            # In the future, we can include the datetime and location as embedding, it should be implemented here
+            self.position = nn.Parameter(torch.randn((img_size // patch_size) ** 2 , emb_size))
+            print("self.position", self.position.shape)
+    
+    
+    def forward(self, x: Tensor) ->Tensor:
+        x = self.projection(x)
+        print("The shape after Patching Embedding",x.shape)
+        # add position embedding
+        x += self.position
+        return x
+
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self,
+                 emb_size: int = 768,
+                 num_heads: int = 8,
+                 dropout: float = 0):
+        super().__init__()
+
+        self.emb_size = emb_size
+        self.num_heads = num_heads
+        # fuse the queries, keys and values in one matrix
+        self.qkv = nn.Linear(emb_size, emb_size * 3)
+        self.att_drop = nn.Dropout(dropout)
+        self.projection = nn.Linear(emb_size, emb_size)
+
+
+    def forward(self, x:Tensor, mask:Tensor=None)->Tensor:
+
+        qkv = rearrange(self.qkv(x), "b n (h d qkv) -> (qkv) b h n d", h=self.num_heads, qkv=3)
+        queries, keys, values = qkv[0], qkv[1], qkv[2]
+        # sum up over the last axis
+        energy = torch.einsum('bhqd, bhkd -> bhqk', queries, keys)# batch, num_heads, query_len, key_len
+        if mask is not None:
+            fill_value = torch.finfo(torch.float32).min
+            energy.mask_fill(~mask, fill_value)
+
+        scaling = self.emb_size ** (1 / 2)
+        att = F.softmax(energy, dim = -1) / scaling
+        att = self.att_drop(att)
+        # sum up over the third axis
+        out = torch.einsum('bhal, bhlv -> bhav ', att, values)
+        out = rearrange(out, "b h n d -> b n (h d)")
+        out = self.projection(out)
+        return out
+
+
+class ResidualAdd(nn.Module):
+    def __init__(self, fn):
+        super().__init__()
+        self.fn = fn
+
+    def forward(self, x, **kwargs):
+        res = x
+        x = self.fn(x, **kwargs)
+        x += res
+        return x
+
+
+class FeedForwardBlock(nn.Sequential):
+    def __init__(self,
+                 emb_size: int,
+                 expansion: int = 4,
+                 drop_p: float = 0.):
+        super().__init__(
+            nn.Linear(emb_size, expansion * emb_size),
+            nn.GELU(),
+            nn.Dropout(drop_p),
+            nn.Linear(expansion * emb_size, emb_size),
+        )
+
+
+class TransformerEncoderBlock(nn.Sequential):
+    def __init__(self,
+                 emb_size: int = 768,
+                 drop_p: float = 0.,
+                 forward_expansion: int = 4,
+                 forward_drop_p: float = 0.,
+                 ** kwargs):
+        super().__init__(
+            ResidualAdd(nn.Sequential(
+                nn.LayerNorm(emb_size),
+                MultiHeadAttention(emb_size, **kwargs),
+                nn.Dropout(drop_p)
+            )),
+            ResidualAdd(nn.Sequential(
+                nn.LayerNorm(emb_size),
+                FeedForwardBlock(
+                    emb_size, expansion=forward_expansion, drop_p=forward_drop_p),
+                nn.Dropout(drop_p)
+            )
+            ))
+
+
+class TransformerEncoder(nn.Sequential):
+    def __init__(self, depth: int = 12, **kwargs):
+        super().__init__(*[TransformerEncoderBlock(**kwargs) for _ in range(depth)])
+
+
+class Upsample(nn.Sequential):
+    """Upsample module.
+    Args:
+        scale (int): Scale factor. Supported scales: 2^n and 3.
+        num_feat (int): Channel number of intermediate features.
+    """
+
+    def __init__(self, scale: int=10):
+        m = []
+        #m.append(nn.Conv2d(in_channels=num_feat, out_channels=scale*scale*num_feat, kernel_size=3, stride=1, padding=1))
+        m.append(nn.PixelShuffle(scale))
+        super(Upsample, self).__init__(*m)
+
+
+
+class TransformerSR(nn.Module):
+
+    def __init__(self, embed_dim: int = 64 ,
+                 img_size: int = 16, upscale: int = 10, patch_size: int=4,
+                 in_channels: int = 8, out_channels: int = 1):
+        print("Transformer Build")
+        super(TransformerSR, self).__init__()
+        print("Building TransformerSR")
+        self.patch_size = patch_size
+        self.img_size = img_size
+        self.upscale = upscale
+        self.out_channels = out_channels
+        
+        self.embed = PatchEmbedding(in_channels, patch_size, embed_dim, img_size=img_size)
+        self.TransformerEncode = TransformerEncoder(depth=2)
+        # for SR
+        #self.conv_before_upsample = nn.Sequential(nn.Conv2d(embed_dim, num_feat, 3, 1, 1),
+        #                                             nn.LeakyReLU(inplace = True))
+
+        self.linear = nn.Linear(embed_dim, patch_size*patch_size*out_channels*upscale*upscale) 
+        self.upsample = Upsample(scale=upscale)
+        self.conv_last = nn.Conv2d(out_channels, 1, 3, 1, 1)
+
+
+    def forward(self,x):
+        x = self.embed(x)
+        x = self.TransformerEncode(x)
+        print("x shape after TransformerEncode:",x.shape)
+        x = self.linear(x)
+        x = x.permute(0, 2, 1) #put channle to the second place
+        print("x shape after permute",x.shape)
+        x = x.reshape(x.size(dim=0), self.out_channels*self.upscale*self.upscale, self.img_size, self.img_size)
+        x = self.upsample(x)
+        print("X shape after upsample",x.shape)
+        #x = self.conv_last(x)
+        return x
+
+
+
+
