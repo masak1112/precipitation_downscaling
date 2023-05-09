a6ba5e15e07d8c0e19f77d0160eb8518b14c10f5
masak1112 <gongbing1112@gmail.com>
2023-05-09 17:11:28 +0200

    revise run_Script_train_swinir_temp_booster

--- HPC_scripts/run_script_train_swinir_temp_booster.sh
+++ HPC_scripts/run_script_train_swinir_temp_booster.sh
@@ -20,18 +20,10 @@ source ../env_setup/venv_booster/bin/activate
 
 train_dir=/p/scratch/deepacf/maelstrom/maelstrom_data/ap5_michael/preprocessed_era5_crea6/t2m/all_files/downscaling_tier2_train.nc
 val_dir=/p/scratch/deepacf/maelstrom/maelstrom_data/ap5_michael/preprocessed_era5_crea6/t2m/all_files/downscaling_tier2_val.nc
-save_dir=../results/exp_20230403_swinIR_temp_remove_last_step
-# save_dir=/p/scratch/deepacf/deeprain/ji4/Downsacling/results/swinUnet_exp1017_origin_booster_3years
-#train_dir=/p/home/jusers/gong1/juwels/scratch_bing/datasets/precip_dataset/train
-#val_dir=/p/home/jusers/gong1/juwels/scratch_bing/datasets/precip_dataset/val
-#save_dir=../results/exp_20230310_unet_booster
+save_dir=../results/exp_20230503_swinIR_temp_remove 
 
 epochs=4
 dataset_type=temperature
-#dataset_type=precipitation
-#model_type=vitSR
 model_type=swinIR
-#model_type=unet
-#model_type=swinUnet
 srun --overlap python ../main_scripts/main_train.py --dataset_type ${dataset_type} --train_dir ${train_dir} --val_dir ${val_dir} --save_dir ${save_dir} --epochs ${epochs} --model_type ${model_type}  
 #srun --overlap python ../main_scripts/dataset_temp.py
