00591026b84232fb878d2e559c23bc501293c00f
masak1112 <gongbing1112@gmail.com>
2024-02-26 01:55:09 +0100

    update runscript

--- HPC_scripts/run_script_test_diffusion_precip_booster.sh
+++ HPC_scripts/run_script_test_diffusion_precip_booster.sh
@@ -19,9 +19,9 @@ source ../sc_venv_template/venv/bin/activate
 
 train_dir=/p/scratch/deepacf/maelstrom/maelstrom_data/ap5/downscaling_precipitation/precip_dataset/train
 test_dir=/p/scratch/deepacf/maelstrom/maelstrom_data/ap5/downscaling_precipitation/precip_dataset/test #test
-checkpoint=../results/exp_20231203_diffusion_t4_newscale2_only_rain_t02_yan_zscore_k095_top/159140_G.pth
+checkpoint=../results/exp_20231203_diffusion_t4_newscale2_only_rain_t02_yan_zscore_k08_top/159140_G.pth
 #checkpoint=../results/exp_20230828_diffusion/3000_G.pth
-save_dir=../results/exp_20231203_diffusion_t4_newscale2_only_rain_t02_yan_zscore_k095_top_test
+save_dir=../results/exp_20231203_diffusion_t4_newscale2_only_rain_t02_yan_zscore_k08_top_test
 model_type=diffusion
 
 srun --overlap python ../main_scripts/main_test.py --test_dir ${test_dir} --stat_dir ${train_dir} --checkpoint ${checkpoint}  --save_dir ${save_dir} --model_type ${model_type} 
