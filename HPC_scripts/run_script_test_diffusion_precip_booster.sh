499de7e42761756913a9e522fda7e20b099e67a8
masak1112 <gongbing1112@gmail.com>
2024-03-01 03:51:18 +0100

    udpate run_script_test_diffusion_precip_booster.sh

--- HPC_scripts/run_script_test_diffusion_precip_booster.sh
+++ HPC_scripts/run_script_test_diffusion_precip_booster.sh
@@ -4,9 +4,9 @@
 #SBATCH --ntasks=1
 #SBATCH --output=test-diffusion-out.%j
 #SBATCH --error=test-diffusion-err.%j
-#SBATCH --time=01:40:00
+#SBATCH --time=05:50:00
 #SBATCH --gres=gpu:1
-#SBATCH --partition=develbooster
+#SBATCH --partition=booster
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=b.gong@fz-juelich.de
 
@@ -18,9 +18,9 @@ module load xarray/0.20.1
 source ../sc_venv_template/venv/bin/activate
 
 train_dir=/p/scratch/deepacf/maelstrom/maelstrom_data/ap5/downscaling_precipitation/precip_dataset/train
-test_dir=/p/scratch/deepacf/maelstrom/maelstrom_data/ap5/downscaling_precipitation/precip_dataset/test_small
-checkpoint=../results/exp_20230828_diffusion/6000_G.pth
-save_dir=../results/exp_20230828_diffusion_results
+test_dir=/p/scratch/deepacf/maelstrom/maelstrom_data/ap5/downscaling_precipitation/precip_dataset/test
+checkpoint=../results/exp_20231203_diffusion_t4_newscale2_only_rain_t02_yan_zscore_05/149000_G.pth
+save_dir=../results/exp_20231203_diffusion_t4_newscale2_only_rain_t02_yan_zscore_05_test
 model_type=diffusion
 
 srun --overlap python ../main_scripts/main_test.py --test_dir ${test_dir} --stat_dir ${train_dir} --checkpoint ${checkpoint}  --save_dir ${save_dir} --model_type ${model_type} 
