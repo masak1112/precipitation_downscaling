93c21e2be44749f42d98924e7458700b439bd903
zhongxin <xinz69560@gmail.com>
2024-10-17 03:18:21 +0000

    " "

--- precipitation_downscaling-bing_issue09_bias_correction/train_scripts/train.py
+++ precipitation_downscaling-bing_issue09_bias_correction/train_scripts/train.py
@@ -33,7 +33,7 @@ class Weight_Loss(nn.Module):
         super(Weight_Loss, self).__init__()
     def init_w(self,y_true):
         weights = torch.tensor(y_true,requires_grad=False) # 
-        MIN = torch.tensor(np.log(1 + 0.1),dtype = weights.dtype,requires_grad=False)
+        MIN = torch.tensor(np.log(1 + 1.7),dtype = weights.dtype,requires_grad=False)
         MAX = torch.tensor(np.log(1 + 80),dtype = weights.dtype,requires_grad=False)
         weights[y_true < MIN] = MIN 
         weights[y_true >= MAX] = MAX
