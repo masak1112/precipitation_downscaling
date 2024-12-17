6c8d04eeed6cd5497601fca5fe22f5ab65e81490
zhongxin <xinz69560@gmail.com>
2024-12-17 15:58:22 +0000

    "ex31_1_onestepwgan_cri5_181_025_l1_aheadtop_0.008_130epoch"

--- precipitation_downscaling-bing_issue09_bias_correction/train_scripts/wgan_train.py
+++ precipitation_downscaling-bing_issue09_bias_correction/train_scripts/wgan_train.py
@@ -26,32 +26,32 @@ cuda = True if torch.cuda.is_available() else False
 Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
 import matplotlib.pyplot as plt
 
-class Weight_Loss(nn.Module):
-    def __init__(self):
-        super(Weight_Loss, self).__init__()
-    def init_w(self,y_true):
-        weights = torch.tensor(y_true,requires_grad=False) # 
-        thresholds = torch.tensor(np.log(1 + np.array([1.2, 4.2, 8])/0.008), dtype=weights.dtype, requires_grad=False) #1.5 5 10
-        weights[y_true < thresholds[0]] = 1
-        weights[(y_true >= thresholds[0]) & (y_true < thresholds[1])] = 10 #10 #8 #6 #3.5 #2
-        weights[(y_true >= thresholds[1]) & (y_true < thresholds[2])] = 80 #80 #50 #20 #15 #6 #3
-        weights[y_true >= thresholds[2]] = 150 #150 #100 #60 #50 #12 #8
-        return weights.to('cuda') 
-    def forward(self, pred, target):
-        error = torch.abs(pred - target)  # L1
-        #error = torch.pow(error, 2)  # L2
-        w = self.init_w(target)
-        return torch.mean(w * error)
+# class Weight_Loss(nn.Module):
+#     def __init__(self):
+#         super(Weight_Loss, self).__init__()
+#     def init_w(self,y_true):
+#         weights = torch.tensor(y_true,requires_grad=False) # 
+#         thresholds = torch.tensor(np.log(1 + np.array([1.2, 4.2, 8])/0.008), dtype=weights.dtype, requires_grad=False) #1.5 5 10
+#         weights[y_true < thresholds[0]] = 1
+#         weights[(y_true >= thresholds[0]) & (y_true < thresholds[1])] = 10 #10 #8 #6 #3.5 #2
+#         weights[(y_true >= thresholds[1]) & (y_true < thresholds[2])] = 80 #80 #50 #20 #15 #6 #3
+#         weights[y_true >= thresholds[2]] = 150 #150 #100 #60 #50 #12 #8
+#         return weights.to('cuda') 
+#     def forward(self, pred, target):
+#         error = torch.abs(pred - target)  # L1
+#         #error = torch.pow(error, 2)  # L2
+#         w = self.init_w(target)
+#         return torch.mean(w * error)
     
-def recon_loss(real_data, gen_data):
-    weighted_loss = Weight_Loss()
-    return weighted_loss(gen_data, real_data)
-
 # def recon_loss(real_data, gen_data):
-#     # initialize reconstruction loss
-#     b_loss = torch.mean(torch.abs(gen_data - real_data))
-#     rloss = torch.abs(gen_data - real_data).mean()
-#     return rloss
+#     weighted_loss = Weight_Loss()
+#     return weighted_loss(gen_data, real_data)
+
+def recon_loss(real_data, gen_data):
+    # initialize reconstruction loss
+    b_loss = torch.mean(torch.abs(gen_data - real_data))
+    rloss = torch.abs(gen_data - real_data).mean()
+    return rloss
 
 
 def get_lr_decay(hparams: dict = None):
