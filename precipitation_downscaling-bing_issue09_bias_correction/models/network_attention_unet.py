a72bc3760eab758961f1293cff0301976b2962e9
masaki1112 <gongbing1112@gmail.com>
2025-01-06 19:02:43 -0500

    add attention unet

---
+++ precipitation_downscaling-bing_issue09_bias_correction/models/network_attention_unet.py
@@ -0,0 +1,323 @@
+__email__ = "gongbing1112@gmail.com"
+__author__ = "Bing Gong"
+__date__ = "2025-01-03"
+
+import torch
+import torch.nn as nn
+import numpy as np
+from torch import Tensor
+from functools import reduce
+from operator import __add__
+
+class Conv2dSamePadding(nn.Conv2d):
+    def __init__(self,*args,**kwargs):
+        super(Conv2dSamePadding, self).__init__(*args, **kwargs)
+        self.zero_pad_2d = nn.ZeroPad2d(reduce(__add__,
+            [(k // 2 + (k - 2 * (k // 2)) - 1, k // 2) for k in self.kernel_size[::-1]]))
+
+    def forward(self, input):
+        return  self._conv_forward(self.zero_pad_2d(input), self.weight, self.bias)
+
+
+class Upsampling(nn.Module):
+
+    def __init__(self, in_channels:int = None, out_channels: int = None,
+                 kernel_size: int = 4, padding: int = 1, stride: int = 2,
+                 upsampling: bool = True, sf: int = 10, mode: str = "bicubic"):
+        super().__init__()
+        """
+        This block is used for transposed low-resolution to the same dim as high-resolution before performing UNet
+        Note: The input data is assumed to be of the form minibatch x channels x [optional depth] x [optional height] x width.
+        :param in_channels : the number of input variables  
+        :param out_channels: the output channels for each ConvTranspose2D layer
+        :param kernel_size : the kernel size
+        :param padding     : the padding size
+        :param stride      : the stride size
+        :param sf          : the scaling factor (low-resolution to high resolution)
+        :param upsampling  : use upsampling (True) to convert low to high resolution  or use transposed convolutional approach(False)
+        """
+
+        if upsampling:
+            self.deconv_block = nn.Upsample(scale_factor = sf, mode = mode, align_corners = True)
+        else:
+            layers = [nn.ConvTranspose2d(in_channels, in_channels, kernel_size = kernel_size, stride = stride,
+                                         padding = padding) for i in range(3)]
+            layers.append(nn.ConvTranspose2d(in_channels, in_channels, kernel_size = kernel_size, stride = 1,
+                                   padding = padding,  ))
+            self.deconv_block = nn.Sequential(*layers)
+
+    def forward(self, x:Tensor)->Tensor:
+         return self.deconv_block(x)
+
+
+
+class Conv_top(nn.Module):
+
+    def __init__(self, in_channels:int = None, out_channels:int = None, kernel_size: int = 3, bias=True):
+        """
+        The convolutional block consists of one convolutional layer, bach normalization and activation function
+        :param in_channels : the number of input channels
+        :param kernel_size : the kernel size
+        :param padding     : the techniques for padding, either 'same' or 'valid' or integer
+        """
+        super().__init__()
+
+
+        self.conv_block1 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding="same", bias=bias),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+        self.conv_block2= nn.Sequential(
+            nn.Conv2d(out_channels, out_channels*2, kernel_size=kernel_size, padding="same", bias=bias),
+            nn.BatchNorm2d(out_channels*2),
+            nn.ReLU(inplace=True)
+        )
+
+
+        self.conv_block3= nn.Sequential(
+            nn.Conv2d(out_channels*2, out_channels*4, kernel_size=kernel_size, padding="same", bias=bias),
+            nn.BatchNorm2d(out_channels*4),
+            nn.ReLU(inplace=True)
+        )
+
+        self.maxpool_conv = nn.MaxPool2d(2)
+
+    def forward(self, x: Tensor)->Tensor:
+        #add noise
+        shape = x.shape
+        torch.manual_seed(10)
+        noise = torch.randn(shape).to(x.device)
+        x0 = torch.cat([noise, x], dim=1)
+        x1 = self.conv_block1(x0) #16, 8, 160, 160
+        m1 = self.maxpool_conv(x1)
+        x2 = self.conv_block2(m1)
+        m2 = self.maxpool_conv(x2)
+        x3 = self.conv_block3(m2)
+        m3 = self.maxpool_conv(x3)
+        
+        return m3
+
+class Conv_Block(nn.Module):
+
+    def __init__(self, in_channels: int = None, out_channels: int = None,
+                 kernel_size: int = 3, padding: str = "same", bias=True):
+        """
+        The convolutional block consists of one convolutional layer, bach normalization and activation function
+        :param in_channels : the number of input channels
+        :param out_channels: the number of output channels
+        :param kernel_size : the kernel size
+        :param padding     : the techniques for padding, either 'same' or 'valid' or integer
+        """
+        super().__init__()
+        self.conv_block = nn.Sequential(
+            Conv2dSamePadding(in_channels, out_channels, kernel_size=kernel_size, bias=bias),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x: Tensor) -> Tensor:
+        return self.conv_block(x)
+
+
+class Conv_Block_N(nn.Module):
+
+    def __init__(self, in_channels: int = None, out_channels: int = None,
+                 kernel_size: int = 3, padding: str = "same", n: int = 2):
+        """
+
+        :param in_channels : the number of input channels
+        :param out_channels: the number of output channels
+        :param kernel_size : the kernel size
+        :param padding     : the techniques for padding, either 'same' or 'valid' or integer
+        :param n           : the number of convolutional block
+        """
+        super().__init__()
+        n_layers = nn.ModuleList([Conv_Block(in_channels, out_channels, kernel_size=kernel_size, padding=padding, bias=True)])
+        for i in np.arange(n - 1):
+            print("i", i)
+            n_layers.append(Conv_Block(out_channels, out_channels, kernel_size=kernel_size, padding=padding, bias=True))
+
+        self.conv_block_n = nn.Sequential(*n_layers)
+
+    def forward(self, x):
+        return self.conv_block_n(x)
+
+
+class Encoder_Block(nn.Module):
+    """Downscaling with maxpool then convol block"""
+
+    def __init__(self, in_channels: int = None, out_channels: int = None, kernel_maxpool: int = 2,
+                 l_large: bool = True):
+        """
+        One complete encoder-block used in U-net.
+        :param in_channels   : the number of input channels
+        :param out_channels  : the number of ouput channels
+        :param kernel_maxpool: the number of kernel size
+        :param l_large       : flag for large encoder (n consecutive convolutional block)
+        """
+        super().__init__()
+
+        if l_large:
+            self.layer1 = Conv_Block_N(in_channels, out_channels, n=8)
+        else:
+            self.layer1 = Conv_Block_N(in_channels, out_channels, n=1)
+
+        self.maxpool_conv = nn.MaxPool2d(kernel_maxpool)
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = self.layer1(x)
+        e = self.maxpool_conv(x)
+        return x, e
+
+
+class Decode_Block(nn.Module):
+    """Upscaling then double conv"""
+
+    def __init__(self, in_channels: int = None, out_channels: int = None, kernel_size: int = 2,
+                 stride_up: int = 2, padding: str = "same"):
+        super().__init__()
+
+        self.up = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride_up, padding=0)
+        self.conv = Conv_Block_N(in_channels, out_channels, n=2, kernel_size=(3, 3), padding=padding)
+
+    def forward(self, x1: Tensor, x2: Tensor) -> Tensor:
+        x1 = self.up(x1)
+        x = torch.cat([x2, x1], dim=1)  # 480
+        return self.conv(x)
+
+
+
+class AttentionBlock(nn.Module):
+    """
+    Tutorial can be found: https://www.youtube.com/watch?v=KOF38xAvo8I
+    https://www.kaggle.com/code/truthisneverlinear/attention-u-net-pytorch
+    """
+    def __init__(self, f_g, f_l, f_int):
+        super().__init__()
+        
+        #Getting gateing signal to the same number of filters as the iter_shape
+        self.w_g = nn.Sequential(
+                                nn.Conv2d(f_g, f_int,
+                                         kernel_size=1, stride=1,
+                                         padding=0, bias=True),
+                                nn.BatchNorm2d(f_int)
+        )
+        
+        #Getting x signal to the same shape as gating signal
+        self.w_x = nn.Sequential(
+                                nn.Conv2d(f_l, f_int,
+                                         kernel_size=1, stride=1,
+                                         padding=0, bias=True),
+                                nn.BatchNorm2d(f_int)
+        )
+        
+        self.psi = nn.Sequential(
+                                nn.Conv2d(f_int, 1,
+                                         kernel_size=1, stride=1,
+                                         padding=0,  bias=True),
+                                nn.BatchNorm2d(1),
+                                nn.Sigmoid(),
+        )
+        
+        self.relu = nn.ReLU(inplace=True)
+
+    
+
+    def forward(self, g, x):
+        g1 = self.w_g(g)
+        x1 = self.w_x(x)
+        #Concatenate and apply relu function
+        psi = self.relu(g1+x1)
+        psi = self.psi(psi)
+
+        return psi*x
+
+
+class AttentionUNet(nn.Module):
+    def __init__(self, n_channels, channels_start: int = 56, dataset_type: str = 'precipitation'):
+        super(UNet, self).__init__()
+        self.dataset_type = dataset_type
+        self.upsampling = Upsampling(n_channels, channels_start,upsampling=True)
+       
+        """encoder """
+        self.down1 = Encoder_Block(n_channels+1, channels_start)
+        self.down2 = Encoder_Block(channels_start, channels_start * 2, l_large=False)
+        self.down3 = Encoder_Block(channels_start * 2, channels_start * 4, l_large=False)
+
+        """ bridge encoder <-> decoder """
+        self.b1 = Conv_Block(channels_start * 4, channels_start * 8)
+    
+        #Topography encoder 
+        self.top = Conv_top(in_channels=2, out_channels=8, kernel_size=3, bias=True)
+
+
+        """decoder """
+        # +++32
+        self.up1 = Decode_Block(channels_start * 8, channels_start * 4)
+        self.att1 = self.AttentionBlock(f_g=channels_start * 4, f_l=channels_start * 4, f_int=channels_start * 2)
+        self.up2 = Decode_Block(channels_start * 4, channels_start * 2)
+        self.att2 = self.AttentionBlock(f_g=channels_start * 2, f_l=channels_start * 2, f_int=channels_start * 1)
+        self.up3 = Decode_Block(channels_start * 2, channels_start)
+        self.att3 = self.AttentionBlock(f_g=channels_start , f_l=channels_start , f_int=channels_start )
+        self.output = nn.Conv2d(channels_start, 1, kernel_size=1, bias=True)
+        torch.nn.init.xavier_uniform(self.output.weight)
+
+
+    def forward(self, x: Tensor, topography: Tensor) -> Tensor:
+        # x = x.cuda()
+        # print("input shape",x.shape)
+        if self.dataset_type == 'precipitation':
+            x = self.upsampling(x)
+            # print("x shape:",x.shape)
+        # remove top  
+        # topography = nn.functional.interpolate(topography, scale_factor=1)
+        top = nn.functional.interpolate(topography, scale_factor=1)
+        x = torch.cat((x, top), 1)
+        # print("x shape:",x.shape)
+
+        s1, e1 = self.down1(x)
+        # print("e1 shape:", e1.shape)
+        s2, e2 = self.down2(e1)
+   
+        s3, e3 = self.down3(e2)
+        # print("e3 shape:", e3.shape)
+        x4 = self.b1(e3)  # -1,448,2,2
+        # print("x4 shape:", x4.shape)
+        # top = self.top(topography) # -1,32,2,2
+        # print("top shape:",top.shape)
+        # #add the topograph to the neural network
+
+        # x5 = torch.cat((x4, top), 1) #-1，480， 20，20
+        # print("x5 shape:",x5.shape)
+        # remove top
+        x5 = x4
+        d3 = self.up1(x5, s3)
+        x3 = self.att1(g=d3, x=x5)
+        d3 = torch.concat((x5, d3), dim=1)
+        
+        # print("d1 shape:", d1.shape)
+        d2 = self.up2(d3, s2)
+        x2 = self.att2(g=d2, x=x3)
+        d3 = torch.concat((x2, d2), dim=1)
+
+        # print("d2 shape:", d2.shape)
+        d1 = self.up3(d2, s1)
+        x1 = self.att3(g=d1, x=x2)
+        d3 = torch.concat((x2, d2), dim=1)        
+    
+        output = self.output(d3) 
+        return output
+
+
+# net = UNet(n_channels = 1,)
+
+# x = torch.rand((10,1,16,16))
+# top = torch.rand((10,1,160,160))
+
+# pred = net(x,top)
+# print(pred.shape)
+'''
+--train_dir /cpfs01/projects-HDD/cfff-4a8d9af84f66_HDD/public/ShiXiSheng/yzy/downscaling_precipitation/precip_dataset_new/train --val_dir /cpfs01/projects-HDD/cfff-4a8d9af84f66_HDD/public/ShiXiSheng/yzy/downscaling_precipitation/precip_dataset_new/val/2017-01 --save_dir /cpfs01/projects-HDD/cfff-4a8d9af84f66_HDD/public/ShiXiSheng/yzy/zx_results/ex6 --model_type fc --epochs 50
+'''
