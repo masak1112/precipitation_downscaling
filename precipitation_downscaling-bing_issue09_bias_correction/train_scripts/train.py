98a6b384179bd02a51d1ef348ce107152ed277c4
zhongxin <xinz69560@gmail.com>
2025-01-15 19:56:38 +0000

    "ex14_5_onestep_weightmae_aheadtop_10_addprecipnorm_1_9_80_160"

--- precipitation_downscaling-bing_issue09_bias_correction/train_scripts/train.py
+++ precipitation_downscaling-bing_issue09_bias_correction/train_scripts/train.py
@@ -58,10 +58,10 @@ class Weight_Loss(nn.Module):
         thresholds = torch.tensor(thresholds, dtype=y_true.dtype, requires_grad=False)
         
         weights = torch.ones_like(y_true, requires_grad=False)
-        weights[y_true < thresholds[0]] = 2
-        weights[(y_true >= thresholds[0]) & (y_true < thresholds[1])] = 8
-        weights[(y_true >= thresholds[1]) & (y_true < thresholds[2])] = 70
-        weights[y_true >= thresholds[2]] = 135
+        weights[y_true < thresholds[0]] = 1
+        weights[(y_true >= thresholds[0]) & (y_true < thresholds[1])] = 9
+        weights[(y_true >= thresholds[1]) & (y_true < thresholds[2])] = 80
+        weights[y_true >= thresholds[2]] = 160
 
         return weights.to('cuda')  
 
